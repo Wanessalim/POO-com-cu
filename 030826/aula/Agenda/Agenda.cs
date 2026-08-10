8f134fa44cdbdc61497f9631b03dba52d80934b4
Nessa :) <wanessalim.2006@gmail.com>
2026-08-10 16:46:04 -0300

    Add files via upload

--- 030826/aula/Agenda/Agenda.cs
+++ 030826/aula/Agenda/Agenda.cs
@@ -6,22 +6,24 @@ namespace Agenda
     {
         struct Contato //conjunto de dados relacionado a uma entidade (Contato)
         {
-            public string Nome;
+            public string Nome, Email , Estado;
             public int Idade;
         }
 
         static void Menu(Contato[] Ag, ref int Tam)
         {
-            int Op;
+            int Op, OpImp, Pos;
+            string Estado;
             do
             {
                 Console.Clear();//limpa tela
                 Console.WriteLine("*****Agenda de Contatos*****");
                 Console.WriteLine();
 
-                Console.WriteLine("1 - para cadastrar");
-                Console.WriteLine("2 - Para Imprimir");
-                Console.WriteLine("3 - Para Sair");
+                Console.WriteLine("[1] - para cadastrar");
+                Console.WriteLine("[2] - Para Buscar adulto por estado");
+                Console.WriteLine("[3] - para buscar pessoa mais velha");
+                Console.WriteLine("[4] - Impressão"); //fazer impressão especial.
                 Console.Write("Opção desejada: ");
                 Op = System.Int32.Parse(Console.ReadLine());
                 switch (Op)
@@ -30,9 +32,24 @@ namespace Agenda
                         Cadastrar(Ag, ref Tam);
                         break;
                     case 2:
-                        Imprimir(Ag, Tam);
+                        Console.Write("Qual Estado: ");
+                        Estado = Console.ReadLine();
+                        ImprimirPorIdadeEstado(Ag, Tam, 18 ,Estado);
                         break;
                     case 3:
+                        //vai me obrigar a imprimir so uma pessoa - um registro
+                        Pos = BuscarMaisVelho(Ag, Tam);
+                        if (Pos >= 0) //posição válida!!
+                        {
+                            ImprimirRegistro(Ag, Pos, false);
+                        }
+                        break;
+                    case 4:
+                        Console.Write("1 - Total / 2 - Parcial: ");
+                        OpImp = Int32.Parse(Console.ReadLine());
+                        Imprimir(Ag, Tam, OpImp == 2);
+                        break;
+                    case 5:
                         break;
                     default:
                         Console.WriteLine("Opção Invalida!!");
@@ -40,13 +57,17 @@ namespace Agenda
                 }
 
                 Console.ReadKey(); //espera resposta
-            } while (Op != 3);
+            } while (Op != 5);
         }
 
         static int Cadastrar(Contato[] Ag, ref int  Tam)
         {
             Console.Write("Nome: ");
             Ag[Tam].Nome = Console.ReadLine();
+            Console.Write("E-mail: ");
+            Ag[Tam].Email = Console.ReadLine();
+            Console.Write("Estado: ");
+            Ag[Tam].Estado = Console.ReadLine();
             Console.Write("Idade:");
             Ag[Tam].Idade = System.Int32.Parse(Console.ReadLine());
             Console.WriteLine();
@@ -54,16 +75,60 @@ namespace Agenda
             return Tam;
         }
 
-        static void Imprimir(Contato[] Ag,  int Tam)
+        static void Imprimir(Contato[] Ag,  int Tam, bool Parcial)
         {
             int Cont;
+            
             for(Cont = 0; Cont < Tam; Cont++)
             {
-                Console.WriteLine("Nome: " + Ag[Cont].Nome);
-                Console.WriteLine("Idade: " + Ag[Cont].Idade);
-                Console.WriteLine();
-            };
+                ImprimirRegistro(Ag, Cont, Parcial);
+            }
+            Console.WriteLine();
                 
+        }
+
+        static void ImprimirPorIdadeEstado(Contato[] Ag, int Tam, int Idade ,string Estado)
+        {
+            int Cont;
+            for (Cont = 0; Cont < Tam; Cont++)
+            {
+                if (Ag[Cont].Idade >= Idade && Ag[Cont].Estado == Estado)
+                {
+                    ImprimirRegistro(Ag, Cont, false);
+                    Console.WriteLine();
+                }
+                
+            }
+            Console.WriteLine();
+
+        }
+
+        static void ImprimirRegistro(Contato[] Ag, int Pos, bool Parcial)
+        {
+            Console.Write("Nome: " + Ag[Pos].Nome);
+            Console.Write(" E-mail: " + Ag[Pos].Email);
+            if (!Parcial)
+            {
+                Console.Write(" Estado: " + Ag[Pos].Estado);
+                Console.Write(" Idade: " + Ag[Pos].Idade);
+            }
+            Console.WriteLine();
+        }
+
+        static int BuscarMaisVelho(Contato[] Ag, int Tam)
+        {
+            int Cont, Pos = -1, MaiorIdade = 0;
+
+            for (Cont = 0; Cont < Tam; Cont++)
+            {
+                if(Cont == 0 || Ag[Cont].Idade > MaiorIdade)
+                {
+                    MaiorIdade = Ag[Cont].Idade;
+                    Pos = Cont;
+                }
+            }
+
+            return Pos;
         }
 
         static void Main(string[] args)
