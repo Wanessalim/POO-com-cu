dcff49e30f80430ab7a2dce5aaa865fd4832625e
Nessa :) <wanessalim.2006@gmail.com>
2026-08-24 09:46:14 -0300

    Add files via upload

---
+++ ListaDeExercicioPOO/Dominio Escola/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_Escola
+{
+    public class Conta
+    {
+        public int Numero;
+        public double Limite;
+        public double Saldo;
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Saldo: " + Saldo);
+        }
+    }
+}
